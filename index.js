f41e8e5d26f4ac460afb06ca9b1c4240814f52e3
ryan gendel <ryangendel@ryans-MacBook-Pro.local>
2021-03-30 18:54:52 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express() //sets up application to listen for incoming requests
+
+//app object represents the underlying running express server
+
+app.get("/",(req,res)=>{
+    res.send({hi:"there"})
+})
+
+const PORT = process.env.PORT || 5000 
+app.listen(5000)
